692db270718bd2ded9d5b49ade037e691490aa38
Alvaro Eduardo Menegon Rosario <alvaro.menegon@gmail.com>
2025-06-04 15:18:52 -0300

    feat: alterar para utilizar npm em vez de CDN

--- src/main.js
+++ src/main.js
@@ -8,9 +8,28 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 * e armazenas como objetos de window
 * para serem acessados globalmente
 */
-
-console.log("THREE.js old version:", window.THREE.REVISION);
-console.log("THREE new version:", THREE.REVISION);
-window.THREE = THREE; // Manter assim por enquanto para não quebrar o código existente
-window.OrbitControls = OrbitControls; // E salvar o novo nome também para poder atualizar o código no futuro aos poucos
+console.log("THREE revision: ", THREE.REVISION);
+window.THREE = THREE;
+window.OrbitControls = OrbitControls;
 window.GLTFLoader = GLTFLoader;
+
+function loadScripts(path) {
+    const script = document.createElement('script');
+    script.src = path;
+    script.async = false;
+    script.deferrer = true;
+    document.head.appendChild(script);
+}
+// carregar o restante dos scripts
+
+loadScripts('./src/globals.js'); //variaveis globais
+loadScripts('./src/AI/garbochess.js'); // Carregar o AI do GarboChess
+loadScripts('./src/lib/jquery/jquery-1.9.1.js'); // Carregar o jQuery 
+loadScripts('./src/lib/jquery/jquery-ui-1.10.3.custom.min.js'); // Carregar o jQuery UI
+loadScripts('./src/utils/three-extend.js');
+loadScripts('./src/gui/loading.js');
+loadScripts('./src/core/Cell.js');
+loadScripts('./src/rendering/factory.js');
+loadScripts('./src/utils/pgnParser.js');
+loadScripts('./src/gui/gui.js');
+loadScripts('./src/core/chess.js');
